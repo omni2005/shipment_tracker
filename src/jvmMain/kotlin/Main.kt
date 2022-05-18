cd168431ca0f655be0467e96e4ade578238e2abb
Braeden Grant <bjgrant1996@gmail.com>
2022-05-18 14:16:56 -0600

    Added file reading and coroutines

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,20 +13,39 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.*
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var simulator: TrackingSimulator = TrackingSimulator()
+    simulator.runSimulation()
+
+    var id by remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Row {
+                Text("Please enter a shipping id to track: ")
+            }
+            TextField(id, onValueChange = {id = it})
+            Button({
+            }) {
+                Text("Track")
+            }
         }
     }
 }
+
+/*
+fun main() = runBlocking { // this: CoroutineScope
+    launch { // launch a new coroutine and continue
+        delay(5000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    println("Hello") // main coroutine continues while a previous one is delayed
+}
+ */
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
--- src/jvmMain/kotlin/TrackingSimulator.kt
+++ src/jvmMain/kotlin/TrackingSimulator.kt
@@ -1,3 +1,6 @@
+import java.io.File
+import java.io.InputStream
+
 class TrackingSimulator {
         var shipments = arrayListOf<Shipment>()
 
@@ -10,6 +13,12 @@ class TrackingSimulator {
         }
 
         fun runSimulation() {
+                val inputStream: InputStream = File("test.txt").inputStream()
+                val lines = mutableListOf<String>()
 
+                inputStream.bufferedReader().forEachLine { lines.add(it) }
+                val lineList = mutableListOf<List<String>>()
+                lines.forEach{lineList.add(it.split(","))}
+                lineList.forEach{it.forEach{(println(it))}}
         }
 }
